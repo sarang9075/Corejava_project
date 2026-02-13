202413f87411d694648a8e38052af230e4fa2339
sarang gote <saranggote356@gmail.com>
2026-02-13 17:26:39 +0530

    Add files via upload

---
+++ Demo1.java
@@ -0,0 +1,27 @@
+
+public class Demo1 {
+	
+	public static void main(String[] args) {
+		System.out.println("Employee name : sarang");
+		System.out.println(" age : 21");
+		System.out.println(" city: nanded");
+		System.out.println(" cource name : bca");
+		System.out.println(" fathers name : sudhakar");
+		System.out.println("mother name : mamata");
+		System.out.println(" gender : male");
+		System.out.println(" Email Id : saranggote@356gmail.com ");
+		System.out.println("contact number : 9075319904");
+		System.out.println("department : bca");
+		System.out.println(" class name : kiran acedemy");
+		System.out.println(" collage name : gpp collage");
+		System.out.println(" current address : pune ");
+		System.out.println(" birt date :31/07/2004");
+		System.out.println(" state :maharastra");
+		
+		
+		
+	}
+
+	
+	
+}
