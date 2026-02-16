bbe5aafd153ed374648b3d40615a785d928707aa
sarang gote <saranggote356@gmail.com>
2026-02-16 22:24:31 +0530

    Employee.java

---
+++ Employee.java
@@ -0,0 +1,17 @@
+package com.tka;
+
+public class Employee {
+	
+	public static void main(String[] args) {
+		System.out.println("Employee information");
+		System.out.println("Employee name" +" sarang ");
+		System.out.println("Employee age : 22");
+		System.out.println("Employee city "+" nanded ");
+		System.out.println("Employee education "+" Bca ");
+		System.out.println("Employee birt date : 31/07/24");
+		System.out.println("Employee 12th mark : 65 %");
+		System.out.println("Employee 10th mark : 80 %");
+		System.out.println("Employee email id "+" saranggote356@gemail.com");
+	
+	}
+}
