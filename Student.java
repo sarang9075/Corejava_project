a18ea7db81545a204c4e401142b1148a3d601532
sarang gote <saranggote356@gmail.com>
2026-02-17 15:11:20 +0530

    Student.java

---
+++ Student.java
@@ -0,0 +1,14 @@
+package com.tka;
+
+public class Student {
+	public static void main(String[] args) {
+		System.out.println("Student name" +" sarang");
+		System.out.println("Student age : 22");
+		System.out.println("Student mobaile no : 9075319904");
+		System.out.println("Student email id" + " saranggote356@gemail.com");
+		System.out.println("Student 10th mark : 80 %");
+		System.out.println("Student 12th mark : 65 %");
+		
+	}
+}
+	
