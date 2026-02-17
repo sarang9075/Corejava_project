223d99f865ffe30354aedfd6d6522c46ee14f6ac
sarang gote <saranggote356@gmail.com>
2026-02-17 15:10:38 +0530

    Library.java

---
+++ Library.java
@@ -0,0 +1,14 @@
+package com.tka;
+
+public class Library {
+	public static void main(String[] args) {
+		System.out.println("Library name " + "gurukul");
+		System.out.println("Library location"  + " pune maharastra");
+		System.out.println("Total books:1000");
+		System.out.println("Add book : 50");
+		System.out.println("Remove book : 50");
+		System.out.println("Library total book 2025 : 400");
+		System.out.println("Library total book 2026 : 500"); 
+	}
+
+}
