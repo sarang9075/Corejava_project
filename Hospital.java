d2bf0eb89fd8059cb799df0b006961bdc0bca6e5
sarang gote <saranggote356@gmail.com>
2026-02-17 15:09:33 +0530

    Hospital.java

---
+++ Hospital.java
@@ -0,0 +1,16 @@
+package com.tka;
+
+public class Hospital {
+	public static void main(String[] args) {
+		System.out.println("Hospital name" + " sardha hospital");
+		System.out.println("Hospital address" +" pune");
+		System.out.println("Hospital time: 10 AM to 8 pm");
+		System.out.println("Hospital total bed : 300");
+		System.out.println("Hospital total petaint  : 67");
+		System.out.println("Hospital free bed : 133");
+		System.out.println("Hospital total doctor : 50");
+		
+		
+	}
+
+}
