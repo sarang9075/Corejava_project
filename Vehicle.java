88fdaaa9a7a908898107363c62fd0dd1d02c41ad
sarang gote <saranggote356@gmail.com>
2026-02-17 15:12:00 +0530

    Vehicle.java

---
+++ Vehicle.java
@@ -0,0 +1,13 @@
+package com.tka;
+
+public class Vehicle {
+	public static void main(String[] args) {
+		System.out.println("Vehicle number : MH12AB1234");
+		System.out.println("Owner name "+ "sarang");
+		System.out.println("Model name " + "swift");
+		System.out.println("Fule type "+"petrol");
+		System.out.println("Price : 800000");
+		System.out.println("Registered" +"yas");
+	}
+
+}
