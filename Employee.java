08de179cfbd033e4b43717399771aaab2711dab9
sarang gote <saranggote356@gmail.com>
2026-02-18 18:53:53 +0530

    Employee.java

--- Employee.java
+++ Employee.java
@@ -3,15 +3,26 @@ package com.tka;
 public class Employee {
 	
 	public static void main(String[] args) {
-		System.out.println("Employee information");
-		System.out.println("Employee name" +" sarang ");
-		System.out.println("Employee age : 22");
-		System.out.println("Employee city "+" nanded ");
-		System.out.println("Employee education "+" Bca ");
-		System.out.println("Employee birt date : 31/07/24");
-		System.out.println("Employee 12th mark : 65 %");
-		System.out.println("Employee 10th mark : 80 %");
-		System.out.println("Employee email id "+" saranggote356@gemail.com");
+		byte age = 25;
+		short idnumber = 6666;
+		int pincode = 431712;
+		long adharnumber = 45454545434l;
+		
+		float hight = 5.9f;
+		double sallery = 40000.00;
+		
+		char specialsymbol = '$';
+		boolean taskcomplate = true;
+		
+		System.out.println("Employee age is ="+ age);
+		System.out.println("Employee idnumber is = " + idnumber);
+		System.out.println("Employee pincode is ="+ pincode);
+		System.out.println("Employee adhar no is ="+ adharnumber);
+		System.out.println("Employee hight = " + hight);
+		System.out.println("Employee sallery is ="+ sallery);
+		System.out.println("Employe special symbol is ="+ specialsymbol);
+		System.out.println("Employee task complate/noncomplate="+ true);
+		
 	
 	}
 }
