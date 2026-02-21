f2f3ac6d2e5e99d95015b9f4c39abea27375fbee
sarang gote <saranggote356@gmail.com>
2026-02-22 00:05:46 +0530

    Add files via upload

---
+++ MyCalculator.java
@@ -0,0 +1,15 @@
+package com.opration;
+
+public class MyCalculator {
+	public static void main(String[] args) {
+		
+	int num1 = 123;
+	int num2 = 3;
+	System.out.println(num1 + num2);
+	System.out.println(num1 - num2);
+	System.out.println(num1 * num2);
+	System.out.println(num1 / num2);   // Questint
+	System.out.println(num1 % num2); // Remainder
+	}
+
+}
