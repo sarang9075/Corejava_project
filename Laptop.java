98c048c9854144e3e80cb8737ef32996cb68e93f
sarang gote <saranggote356@gmail.com>
2026-02-24 11:51:33 +0530

    Laptop.javaVariable

---
+++ Laptop.java
@@ -0,0 +1,27 @@
+package com.variable;
+
+public class Laptop {
+	public static void main(String[] args) {
+		String brandName = "Dell";
+		String modelName = "XPS 13";
+		int ram = 16;     // GB
+		int storage = 512;  //GB
+		double screenSize = 13.3;
+		String processor = "Intel i7";
+		boolean isTouchscreen = true;
+		double price = 129999.99;
+		
+		System.out.println("Brand Name =" + brandName);
+		System.out.println("modelName =" + modelName);
+		System.out.println(" Laptop Ram =" + ram );
+		System.out.println("Laptop Storage =" + storage);
+		System.out.println("Laptop Screen Size = " + screenSize);
+		System.out.println("Laptop Processor =" + processor);
+		System.out.println(" Laptop is touch screen =" + isTouchscreen);
+		System.out.println(" Laptop Price =" + price);
+		
+		
+		
+	}
+
+}
