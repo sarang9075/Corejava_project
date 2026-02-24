ebbe196a1dece212501d51f9e6cb8a1cbdb711ca
sarang gote <saranggote356@gmail.com>
2026-02-24 19:11:35 +0530

    Add files via upload

---
+++ Default Size/Employee.java
@@ -0,0 +1,24 @@
+package com.defaultsize;
+
+public class Employee {
+static String employeeName = "sarang" ;
+static byte empAge  = 22;
+static int empIdnumber = 43434 ;
+static long empAdharNo = 43564636758l;
+static float empHight = 5.5f;
+static double empSalary;
+static boolean empFresher = true;
+
+public static void main(String[] args) {
+	System.out.println(employeeName);
+	System.out.println(empAge);
+	System.out.println(empIdnumber);
+	System.out.println(empAdharNo);
+	System.out.println(empHight);
+	System.out.println(empSalary);
+	System.out.println(empFresher);
+	
+}
+
+
+}
