277f8ded66663577a1a386e4bbaf4577af97d795
sarang gote <saranggote356@gmail.com>
2026-02-25 00:05:17 +0530

    Add files via upload

---
+++ Default Size/Vehicle.java
@@ -0,0 +1,26 @@
+package com.defaultsize;
+
+public class Vehicle {
+static int vehicleID = 1;
+static String vehicleName = "car";
+static String brandName = "Tata";
+static double vehiclePrice = 800000.00;
+static int modelYear = 2022;
+static String color = "White";
+static int serviceCount;  //new car
+static double mileageRecord;
+
+public static void main(String[] args) {
+	System.out.println(vehicleID);
+	System.out.println(vehicleName);
+	System.out.println(brandName);
+	System.out.println(vehiclePrice);
+	System.out.println(modelYear);
+	System.out.println(color);
+	System.out.println(serviceCount);
+	System.out.println(mileageRecord);
+}
+
+
+
+}
