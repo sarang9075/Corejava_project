18432c9db8da21d8f2bc5ca3a52545b08a39d877
sarang gote <saranggote356@gmail.com>
2026-02-24 11:50:41 +0530

    City.javaVariable

---
+++ City.java
@@ -0,0 +1,24 @@
+package com.variable;
+
+public class City {
+	public static void main(String[] args) {
+		String cityName = "pune";
+		String country = "INDIA";
+		int population = 3500000;
+		double cityArea = 331.3;
+		boolean hasAirport = true;
+		String state = "Maharastra";
+		boolean hasMetro = true;
+		
+		System.out.println("City name =" + cityName);
+		System.out.println("Country =" + country);
+		System.out.println(" City population = " + population);
+		System.out.println("City Area =" + cityArea);
+		System.out.println("has Airport =" + hasAirport);
+		System.out.println(" Which State =" + state);
+		System.out.println(" Has Metro = " + hasMetro);
+		
+		
+	}
+
+}
