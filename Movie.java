6d0aab28ce90055ba2c6d5af3adcbdd9d83a6114
sarang gote <saranggote356@gmail.com>
2026-02-24 11:52:51 +0530

    Movie.javaVariable

---
+++ Movie.java
@@ -0,0 +1,21 @@
+package com.variable;
+
+public class Movie {
+public static void main(String[] args) {
+	String movieName = "3 Idiots";
+	String directorName = " Rajkumar Hirani ";
+	int releaseYear = 2009;
+	double rating = 8.4;
+	boolean isAvailableonline = true;
+	String language = "Hindi";
+	int awardswon = 6;
+	
+	System.out.println("Movie Name =" + movieName);
+	System.out.println("Director Name =" + directorName);
+	System.out.println("Release Year =" + releaseYear);
+	System.out.println("Movie Rating =" + rating);
+	System.out.println("Movie Available Online =" + isAvailableonline);
+	System.out.println("Movie language =" +  language);
+	System.out.println(" Number of  Awards Wons movie =" +  awardswon);
+}
+}
