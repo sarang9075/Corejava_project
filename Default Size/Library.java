29ce3c7cb9f7789abfec648349a1743a4beb471f
sarang gote <saranggote356@gmail.com>
2026-02-25 00:04:22 +0530

    Add files via upload

---
+++ Default Size/Library.java
@@ -0,0 +1,20 @@
+package com.defaultsize;
+
+public class Library {
+static String libraryName = "Siksha";
+static String libraryLocation= "Pune maharastra";
+static int libraryTotal_Book = 600;
+static int addBook;
+static int removeBook;
+static boolean library_Opening_in1991;
+
+public static void main(String[] args) {
+	System.out.println(libraryName);
+	System.out.println(libraryLocation);
+	System.out.println(libraryTotal_Book);
+	System.out.println(addBook);
+	System.out.println(removeBook);
+	System.out.println(library_Opening_in1991);
+}
+
+}
