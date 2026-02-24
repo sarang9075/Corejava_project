ff9aefbba93005d780d4cd1f52ed6be3cbda5f49
sarang gote <saranggote356@gmail.com>
2026-02-24 11:52:13 +0530

    Mobile.javaVriable

---
+++ Mobile.java
@@ -0,0 +1,27 @@
+package com.variable;
+
+public class Mobile {
+	public static void main(String[] args) {
+		String brandName = "Samsung";
+		String model = "Galaxy S23";
+		double screenSize = 6.8;
+		int ram = 12;       // GB
+		int mobileStorage = 256;    // GB
+		String opratingSystem = "android";
+		boolean mobile5g = true;
+		String mobileColor = "Black";
+		double price = 79999.99;
+		
+		System.out.println("Brand name is =" + brandName);
+		System.out.println("Model name is =" + model);
+		System.out.println("Mobile screen size = " + screenSize);
+		System.out.println("Mobile Ram is" + ram);
+		System.out.println("Mobile storage " + 256);
+		System.out.println("Oprating system =" +opratingSystem);
+		System.out.println("5G Mobile =" + mobile5g );
+		System.out.println("Mobile color =" + mobileColor);
+		System.out.println("Mobile price =" + price);
+	}
+	
+
+}
