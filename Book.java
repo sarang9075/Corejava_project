910a50987a7c41f291965470afdb1664e7f22b24
sarang gote <saranggote356@gmail.com>
2026-02-24 11:48:55 +0530

    Book.java variable

---
+++ Book.java
@@ -0,0 +1,25 @@
+package com.variable;
+
+public class Book {
+	public static void main(String[] args) {
+		String Title = "winges of fire";
+		String Author = "A.P.J. Abdul kalam";
+		int Pages = 350;
+		int yearPublished = 1999;
+		double Price = 350.50;
+		boolean isBestseller = true;
+		String Language = "English";
+		
+		System.out.println("Book Title name ="+ Title);
+		System.out.println("Book Author Name is="+ Author);
+		System.out.println(" Number of Pages="+ Pages);
+		System.out.println("year Published="+ yearPublished);
+		System.out.println("Book price ="+ Price);
+		System.out.println(" is Bests seller="+ isBestseller);
+		System.out.println("Book language="+ Language);
+		
+		
+		
+	}
+
+}
