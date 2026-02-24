ad09ff5239c0ee3da4c79a82e4aa7a0635cb40d8
sarang gote <saranggote356@gmail.com>
2026-02-25 00:04:51 +0530

    Add files via upload

---
+++ Default Size/Student.java
@@ -0,0 +1,23 @@
+package com.defaultsize;
+
+public class Student {
+static String studentName = "sarang gore";
+static int studentAge = 22;
+static int studentSeatno = 111;
+static long studentAdharno = 454646464646l;
+static float studentHight = 5.5f;
+static int studentMark;    // Result not declared
+static boolean checkPass;
+
+public static void main(String[] args) {
+	System.out.println(studentName);
+	System.out.println(studentAge);
+	System.out.println(studentSeatno);
+	System.out.println(studentAdharno);
+	System.out.println(studentHight);
+	System.out.println(studentMark);
+	System.out.println(checkPass);
+	
+}
+
+}
