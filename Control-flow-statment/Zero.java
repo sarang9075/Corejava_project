02e0a055426b0dc44b693633b0457a30903965ad
sarang gote <saranggote356@gmail.com>
2026-03-14 23:25:14 +0530

    Add files via upload

---
+++ Control-flow-statment/Zero.java
@@ -0,0 +1,15 @@
+package com.controlflow;
+
+public class Zero {
+	public static void main(String[] args) {
+		int a = 0;
+		if (a > 0) {
+			System.out.println("Number is posetive");
+		} else if (a < 0) {
+			System.out.println("Number is Negetive");
+		} else {
+			System.out.println("Number is Zero");
+		}
+	}
+
+}
