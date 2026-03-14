8d038b321d82da9a67a7d02c73ed2946da681ebb
sarang gote <saranggote356@gmail.com>
2026-03-14 23:20:04 +0530

    Add files via upload

---
+++ Control-flow-statment/LargestAmongNumber.java
@@ -0,0 +1,14 @@
+package com.controlflow;
+
+public class LargestAmongNumber {
+public static void main(String[] args) {
+	int a = 45 , b = 15 , c = 10;
+	if (a>b && a>c) {
+		System.out.println(a+ " :-This is  Number Greatest");
+	} else if (b>c) {
+		System.out.println(b+ ":-This is Number Greatest");
+	}else {
+		System.out.println(c+ ":-This is Number Greatest");
+	}
+}
+}
