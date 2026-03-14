006cb8c743a5235e7d70d32acc0d6d922ba3d351
sarang gote <saranggote356@gmail.com>
2026-03-14 23:17:36 +0530

    Add files via upload

---
+++ Control-flow-statment/Divisible3and5.java
@@ -0,0 +1,12 @@
+package com.controlflow;
+
+public class Divisible3and5 {
+public static void main(String[] args) {
+	int a= 25;
+	if (a%3 == 0 && a%5 ==0) {
+		System.out.println("The number is divisible by 5&3");
+	}else {
+		System.out.println("The numer is not divisible by 5&3");
+	}
+}
+}
