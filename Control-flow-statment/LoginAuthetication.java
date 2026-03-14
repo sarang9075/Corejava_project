0e036daf677be6cd3655e09be55c4983c2347dee
sarang gote <saranggote356@gmail.com>
2026-03-14 23:22:18 +0530

    Add files via upload

---
+++ Control-flow-statment/LoginAuthetication.java
@@ -0,0 +1,14 @@
+package com.controlflow;
+
+public class LoginAuthetication {
+	public static void main(String[] args) {
+		String username = "sarang9075";
+		String password = "sarang9075";
+		if (username == password) {
+			System.out.println("Loging succesfully");
+		} else {
+			System.out.println("failed login");
+		}
+
+	}
+}
