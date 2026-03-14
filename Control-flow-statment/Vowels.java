4105972cdd2dd9e1faa9f9c02e4f59b41f734eff
sarang gote <saranggote356@gmail.com>
2026-03-14 23:24:48 +0530

    Add files via upload

---
+++ Control-flow-statment/Vowels.java
@@ -0,0 +1,14 @@
+package com.controlflow;
+
+public class Vowels {
+	public static void main(String[] args) {
+		char a = 'u';
+		if (a == 'a' || a == 'e' || a == 'i' || a == 'o' || a == 'u') {
+			System.out.println("Number is Vowels");
+		} else {
+			System.out.println("Number is Consonant");
+
+		}
+	}
+
+}
