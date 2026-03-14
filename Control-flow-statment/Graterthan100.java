25c1cee84f8dc1ab958f5aba6c9c21f4491b7670
sarang gote <saranggote356@gmail.com>
2026-03-14 23:19:20 +0530

    Add files via upload

---
+++ Control-flow-statment/Graterthan100.java
@@ -0,0 +1,13 @@
+package com.controlflow;
+
+public class Graterthan100 {
+	public static void main(String[] args) {
+		int a = 125;
+		if (a < 100) {
+			System.out.println(a + ":number is less than Hundred");
+		} else {
+			System.out.println(a + ":number is grater than Hundred");
+		}
+
+	}
+}
