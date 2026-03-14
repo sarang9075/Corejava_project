15e5e0f3910a391ad2c2188345164d1692c217dd
sarang gote <saranggote356@gmail.com>
2026-03-14 23:23:39 +0530

    Add files via upload

---
+++ Control-flow-statment/Uppercase.java
@@ -0,0 +1,13 @@
+package com.controlflow;
+
+public class Uppercase {
+	public static void main(String[] args) {
+		char a = 'a';
+		if (a >= 'A' && a <= 'Z') {
+			System.out.println("Charecter is Uppearcase");
+		} else {
+			System.out.println("Charecter is Lowercase");
+		}
+	}
+
+}
