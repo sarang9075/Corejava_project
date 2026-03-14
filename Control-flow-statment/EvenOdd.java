6cb5699c7779352538fb024b50df3a4d32b24a94
sarang gote <saranggote356@gmail.com>
2026-03-14 23:18:35 +0530

    Add files via upload

---
+++ Control-flow-statment/EvenOdd.java
@@ -0,0 +1,14 @@
+package com.controlflow;
+
+public class EvenOdd {
+	public static void main(String[] args) {
+
+		int age = 24;
+		if (age % 2 == 0) {
+			System.out.println("it is even Number");
+		} else {
+			System.out.println("otherwise its odd");
+
+		}
+	}
+}
