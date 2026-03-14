cfc309138a4af49b2a5c5f53aa74729c57b5e5d6
sarang gote <saranggote356@gmail.com>
2026-03-14 23:24:21 +0530

    Add files via upload

---
+++ Control-flow-statment/Voting.java
@@ -0,0 +1,14 @@
+package com.controlflow;
+
+public class Voting {
+	public static void main(String[] args) {
+		int age = 18;
+		if (age >= 18) {
+			System.out.println("person is Eligible for Voting...!");
+		} else {
+			System.out.println("Is not Eligible Voting..!");
+
+		}
+	}
+
+}
