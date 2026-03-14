6cf512d7670d009841fdf9ed1c72d8097917e493
sarang gote <saranggote356@gmail.com>
2026-03-14 23:15:34 +0530

    Add files via upload

---
+++ Control-flow-statment/Compare.java
@@ -0,0 +1,14 @@
+package com.controlflow;
+
+public class Compare {
+	public static void main(String[] args) {
+		int a = 21;
+		int b = 10;
+		if (a > b) {
+			System.out.println(a+"number is greater...!");
+		}  else  {
+			System.out.println(b+"number is greater...!");
+		}
+		}
+	}
+
