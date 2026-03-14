1e651cfac83f3c1fee3ca9460894ece8f8b1dff5
sarang gote <saranggote356@gmail.com>
2026-03-14 23:16:09 +0530

    Add files via upload

---
+++ Control-flow-statment/Day.java
@@ -0,0 +1,21 @@
+package com.controlflow;
+
+public class Day {
+	public static void main(String[] args) {
+		int day = 7;
+		if (day == 1) {
+			System.out.println("The day is Sunday");
+		} else if (day == 2) {
+			System.out.println("The day is Monday");
+		} else if (day == 3) {
+			System.out.println("The day is Tuesday");
+		} else if (day == 4) {
+			System.out.println("The day is Wednesday");
+		} else if (day == 5) {
+			System.out.println("The day is Thursday");
+		} else if (day == 6) {
+			System.out.println("The day is Saturday");
+
+		}
+	}
+}
